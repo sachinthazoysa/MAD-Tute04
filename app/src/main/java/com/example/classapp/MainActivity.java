e19f5543989a46b1214c29d4c431190d5d785429
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2020-03-31 21:31:45 +0530

    changed the code according to lecture video

--- app/src/main/java/com/example/classapp/MainActivity.java
+++ app/src/main/java/com/example/classapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.classapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.service.autofill.OnClickAction;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,21 +14,65 @@ import android.widget.EditText;
 import Database.DBHandler;
 
 public class MainActivity extends AppCompatActivity {
-    Button addbtn;
+    Button addBtn,singBtn,UpdtBtn,dltBtn,slctBtn;
     EditText username;
     EditText password;
+    DBHandler database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database= new DBHandler(this);
         username = findViewById(R.id.editText);
         password = findViewById(R.id.editText2);
-        addbtn=findViewById(R.id.add);
+        addBtn=findViewById(R.id.add);
+        singBtn=findViewById(R.id.singin);
+        UpdtBtn=findViewById(R.id.update);
+        dltBtn=findViewById(R.id.delete);
+        slctBtn=findViewById(R.id.button);
+
     }
 
-    public void onClick(View view){
+    public void onResume() {
+        super.onResume();
+        
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                database.addInfo(username.getText().toString(), password.getText().toString());
+            }
+        });
+
+
+        singBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor newRowId = database.readAllInfo();
+                if(newRowId!= null) {
+                    while (newRowId.moveToNext()) {
+                        Log.d("User record", "User" + newRowId.getString(2) + newRowId.getString(2));
+                    }
+
+                }else
+                    showMessage();
+            }
+
+        });
+
 
     }
+
+    private void showMessage() {
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle("Error");
+        builder.setMessage("No user available");
+        builder.show();
+    }
+
+
+
 
 
 }
